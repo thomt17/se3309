55864ea9acb036c584f7c51f14c7f4f63dd1f739
Nick Ward <nward24@uwo.ca>
2013-11-28 13:41:36 -0500

    added gitignore

--- 3309Assignment4/src/se3309/BaseballBrowserMain.java
+++ 3309Assignment4/src/se3309/BaseballBrowserMain.java
@@ -48,17 +48,6 @@ public class BaseballBrowserMain extends JApplet {
 	       }
 	}
 	public void init() {
-	    //Execute a job on the event-dispatching thread:
-	    //creating this applet's GUI.
-	    /*try {
-	        javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
-	            public void run() {
-	                createGUI();
-	            }
-	        });
-	    } catch (Exception e) {
-	        System.err.println("createGUI didn't successfully complete");
-	    }*/
 	    createGUI();
 	    test();
 	}
